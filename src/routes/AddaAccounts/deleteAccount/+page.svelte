2c25c3b2fd227a98d4a3525c5571a424c596c78a
mister-ritom <ritomghosh856@gmail.com>
2024-05-14 10:52:02 +0530

    Account deletation

---
+++ src/routes/AddaAccounts/deleteAccount/+page.svelte
@@ -0,0 +1,30 @@
+<h1>Delete account information</h1>
+<p>Email ritomghosh856@gmail.com to delete your account information from Adda Server</p>
+<h2>
+    Email Format:
+</h2>
+<p>
+    Subject: Delete Account Information
+    <br/>
+    Text Body:
+      Delete account from Sever with the following information
+    <br/>
+    Required information: 
+</p>
+<ul>
+    <li>Your Name</li>
+    <li>Your Email</li>
+    <li>Your Phone Number</li>
+    <li>Your Account username</li>
+</ul>
+
+<blockquote>
+    <p>
+     Proof can be a screenshot of your account page or any other information that can prove that the account belongs to you.
+    </p>
+    <br/>
+    <p>
+
+        You will receive a confirmation email after your account information is deleted from the server.
+    </p>
+</blockquote>
